477cb918365cbb2c77d1739c47b8a38be00470dc
unknown <jlopezr8895@gmail.com>
2025-08-13 21:40:35 -0500

    Nueva funcion 2.0

--- ejerciciosPython.py
+++ ejerciciosPython.py
@@ -1,2 +1,9 @@
 
 Lista= ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto"]
+notas=[
+    [3.5, 4.0], #Estudiante1
+    [4.2, 3.8], #Estudiante2
+    [2.9, 3.0], #Estudiante3
+]
+
+print("Segunda nota del tercer estudiante", notas[2][1])
